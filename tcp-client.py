59a6e466ffa558fb71614c493cf3d5d1465ab227
paintedblue <pinkclover22@naver.com>
2023-11-12 14:23:16 +0900

    second commit

--- tcp-client.py
+++ tcp-client.py
@@ -5,16 +5,18 @@ import socket
 IP = input("Server address: ")
 TCP_PORT = 5000
 BUFFER_SIZE = 1024
-MESSAGE = ""
 
-while True:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((IP, TCP_PORT))
-    MESSAGE = input("Write to server: ")
-    if MESSAGE == 'q':
-        break
-    s.send(MESSAGE.encode('utf-8'))
-    print('Sending data:', MESSAGE)
-    data = s.recv(BUFFER_SIZE)
-    print('Data from server:', data.decode('utf-8'))
-    s.close()
+    print("Connected to server.")
+    
+    while True:
+        MESSAGE = input("Write to server (type 'q' to quit): ")
+        if MESSAGE.lower() == 'q':  # Allows 'Q' or 'q' to quit
+            break
+        s.send(MESSAGE.encode('utf-8'))
+        print('Sending data:', MESSAGE)
+        data = s.recv(BUFFER_SIZE)
+        print('Data from server:', data.decode('utf-8'))
+
+    print("Connection closed.")
--- tcp-server.py
+++ tcp-server.py
@@ -2,27 +2,34 @@
 # test5
 import socket
 import time
+import threading
 
 IP_ADDR = '0.0.0.0'
 TCP_PORT = 5000
 BUFFER_SIZE = 1024
 
+def handle_client_connection(client_socket, address):
+    print(f'New connection from {address}')
+    while True:
+        data = client_socket.recv(BUFFER_SIZE)
+        if not data:
+            break  # 클라이언트가 연결을 종료한 경우
+        currentTime = " " + "updated !!!" + time.ctime(time.time()) + "\r\n"
+        data = data.decode('utf-8') + currentTime
+        client_socket.send(data.encode('ascii'))
+    client_socket.close()
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((IP_ADDR, TCP_PORT))
 s.listen(1)
+print("Waiting for connections...")
 
 try:
     while True:
-        conn, addr = s.accept()
-        print('Client address:', addr)
-        data = conn.recv(BUFFER_SIZE)
-        if not data:
-            break
-        currentTime = " " + "updated !!!" + time.ctime(time.time()) + "\r\n"
-        data = data.decode('utf-8') + currentTime
-        conn.send(data.encode('ascii'))
-        conn.close()
+        client_conn, client_addr = s.accept()
+        client_thread = threading.Thread(target=handle_client_connection, args=(client_conn, client_addr))
+        client_thread.start()
 except KeyboardInterrupt:
     s.close()
     print("Server stopped by user")
