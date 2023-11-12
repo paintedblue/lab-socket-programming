b6bb0d33f3dd65f0551129a4360e7f9437d4c06f
paintedblue <pinkclover22@naver.com>
2023-11-12 21:29:48 +0900

    third commit

--- tcp-client.py
+++ tcp-client.py
@@ -2,19 +2,39 @@
 # -*- coding:utf-8 -*-
 import socket
 
-IP = input("Server address: ")
-TCP_PORT = 5005
-BUFFER_SIZE = 1024
-MESSAGE = ""
+def main():
+    server_ip = input("Server address: ")
+    tcp_port = 5005
+    buffer_size = 1024
 
-while True:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((IP, TCP_PORT))
-    MESSAGE = input("Write to server: ")
-    if MESSAGE == 'q':
-        break
-    s.send(MESSAGE.encode('utf-8'))
-    print('Sending data:', MESSAGE)
-    data = s.recv(BUFFER_SIZE)
-    print('Data from server:', data.decode('utf-8'))
-    s.close()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.connect((server_ip, tcp_port))
+            print(f"Connected to server {server_ip} on port {tcp_port}.")
+            
+            while True:
+                message = input("Write to server: ")
+                if message.lower() == 'bye':  # 'bye' 메시지를 사용하여 종료합니다.
+                    print("Closing connection...")
+                    s.sendall(message.encode('utf-8'))  # 'bye' 메시지를 서버에게 보냅니다.
+                    break
+                s.sendall(message.encode('utf-8'))
+                print('Sending data:', message)
+                data = s.recv(buffer_size)
+                print('Data from server:', data.decode('utf-8'))
+                if not data:
+                    # 데이터 수신이 없으면, 서버가 연결을 닫았다고 가정합니다.
+                    print("Server closed the connection.")
+                    break
+
+        except socket.error as e:
+            print(f"Socket error occurred: {e}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        finally:
+            # 연결을 정리합니다.
+            s.close()
+            print("Connection closed.")
+
+if __name__ == "__main__":
+    main()
